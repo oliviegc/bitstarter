2508bd42bb22d48d2f1b90122989b1b5d7e13b99
Gerhard Olivier <golivier@global.co.za>
2013-07-10 13:42:16 +0200

    web.js Reading message from file

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var fs = require('fs');
+
+var getData = function(filename,response) {
+  fs.readFile(filename,function(err,data){
+    if (err) throw err;
+    //console.log(data);
+    response.send(data.toString('utf-8'));
+    return data;
+ });
+};
+
+var d = new Buffer(1000);
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  getData('index.html',response);
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
